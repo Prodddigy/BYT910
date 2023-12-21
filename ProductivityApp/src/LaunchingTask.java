78a6c7b0fada7da340a16e65ea01ad3e76a13e77
Damian Czarnecki <deadsonofprejudice@outlook.com>
2023-12-21 21:14:14 +0100

    work in progress, wierzę w was chłopaki

--- ProductivityApp/src/LaunchingTask.java
+++ ProductivityApp/src/LaunchingTask.java
@@ -2,16 +2,47 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class LaunchingTask {
+
+    private User user;
+    private User.Task task;
+
     private final LocalDateTime StartDateTime;
    private LocalDateTime EndDateTime =null;
 
    private Duration duration=null;
 
-   LaunchingTask()
+
+
+    private String time;
+   LaunchingTask(String time)
    {
+       this.time = time;
        this.StartDateTime = LocalDateTime.now();
    }
 
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User.Task getTask() {
+        return task;
+    }
+
+    public void setTask(User.Task task) {
+        this.task = task;
+    }
 
     public LocalDateTime getStartDateTime() {
         return StartDateTime;
@@ -39,6 +70,10 @@ public class LaunchingTask {
         {
             this.duration = Duration.between(StartDateTime, EndDateTime);
         }
+    }
+    public void overTimeStop()
+    {
+     //stops when time is passed for the certain task
     }
 
 
